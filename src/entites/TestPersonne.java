59b72378fca4f6afc18019433de0c35a3d6ac6b7
Odrey142 <106230623+Odrey142@users.noreply.github.com>
2022-10-10 14:38:21 +0200

    commit avce réponse question 7

--- src/entites/TestPersonne.java
+++ src/entites/TestPersonne.java
@@ -6,33 +6,32 @@ public class TestPersonne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		AdressePostale adresse3 = new AdressePostale();
-		adresse3.codePostal = 97200;
-		adresse3.numeroDeRue = 5;
-		adresse3.libelleDeRue = "Fort de france";
-		adresse3.ville = "Martinique";
+		
 			
 		Personne per1 = new Personne();
 			per1.nom = "Gilbert";
 			per1.prenom = "Don";
-			per1.adressePostale = adresse3;
+			per1.adressePostale = new AdressePostale();
+			per1.adressePostale.codePostal = 97200;
+			per1.adressePostale.numeroDeRue = 5;
+			per1.adressePostale.libelleDeRue = "Fort de france";
+			per1.adressePostale.ville = "Martinique";
 			
 
 	AdressePostale adresse4 = new AdressePostale();
-			adresse4.codePostal = 97200;
-			adresse4.numeroDeRue = 5;
-			adresse4.libelleDeRue = "Konoha";
-			adresse4.ville = "Pays du feu";			
+					
 		Personne per2 = new Personne();
 			per2.nom =" Uzumaki";
 			per2.prenom = "Naruto";
-			per2.adressePostale = adresse4;
-			
-
-			
+			per2.adressePostale = new AdressePostale();
+			per2.adressePostale.codePostal = 97200;
+			per2.adressePostale.numeroDeRue = 5;
+			per2.adressePostale.libelleDeRue = "Konoha";
+			per2.adressePostale.ville = "Pays du feu";	
 			
 			
 	}
 
+	//Il y a beaucoup de ligne à coder 
+	
 }
